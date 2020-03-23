e1bbc2822bdd13e80b9d05be12eda8d3296a0b34
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-03-23 18:26:40 +0100

    setup typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Gatsby, Redux-Toolkit and Typescript starter`,
+    description: `Gatsby, Redux-Toolkit and Typescript starter`,
+    author: `Saimir Kapaj`
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -24,11 +25,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 };
