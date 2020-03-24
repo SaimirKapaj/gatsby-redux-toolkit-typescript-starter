b4416a832c06d93250957b6007d81a42cc0c4684
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-03-24 13:39:02 +0100

    removed images into assets folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/assets/images`
       }
     },
     `gatsby-transformer-sharp`,
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`
+        icon: `src/assets/images/gatsby-icon.png`
       }
     }
   ]
